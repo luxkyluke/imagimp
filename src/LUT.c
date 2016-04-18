ddf4560cc4e5f0f3aee58227a9079d46a0e3ed06
luxkyluke <rock_on77500@hotmail.fr>
2016-04-18 17:16:08 +0200

    LUTHIER

--- src/LUT.c
+++ src/LUT.c
@@ -1,13 +1,44 @@
 
 #include "LUT.h"
 
-void MakeLUT(LUT* list){
+LUT* initLUT(){
+	LUT* list = malloc(sizeof(LUT));
 	if(!list)
-		return;
+		return NULL;
 	list->prev = NULL;
 	list->next = NULL;
+	return list;
 }
 
 bool LUTIsEmpty(LUT* list){
 	return (list->next == NULL);
+}
+
+void addNode(LUT* list, int lut[256]){
+	int i;
+	if (list == NULL) return;
+	if (LUTIsEmpty(list)){
+		list->next = list;
+		list->prev = list;
+		for (i = 0; i < 256; i++)
+			list->lut[i] = lut[i];
+		return;
+	}
+	if (list->next == list)	{
+		LUT* newNode = malloc(sizeof(LUT));
+		newNode->prev = list;
+		newNode->next = list;
+		list->next = newNode;
+		list->prev = newNode;
+		for (i = 0; i < 256; i++)
+			newNode->lut[i] = lut[i];
+		return;
+	}
+	LUT* newNode = malloc(sizeof(LUT));
+	newNode->next = list;
+	newNode->prev = list->prev;
+	list->prev->next = newNode;
+	list->prev = newNode;
+	for (i = 0; i < 256; i++)
+		newNode->lut[i] = lut[i];
 }
